e8b9b1853b2ae5cc0bcda5784b35ea48bff922a9
Mervin Fansler <mmfansler@gmail.com>
2020-10-07 02:20:50 -0400

    closes #5

---
+++ extdata/bxs/download_10X_whitelists.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# 10xv1
+wget https://github.com/10XGenomics/supernova/raw/1.2.0/tenkit/lib/python/tenkit/barcodes/737K-april-2014_rc.txt
+
+# 10xv2
+wget https://github.com/10XGenomics/supernova/raw/1.2.0/tenkit/lib/python/tenkit/barcodes/737K-august-2016.txt
+
+# 10xv3
+#wget https://github.com/10XGenomics/supernova/raw/1.2.0/tenkit/lib/python/tenkit/barcodes/4M-with-alts-february-2016.txt
