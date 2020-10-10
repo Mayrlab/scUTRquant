51e61670b0123de547ef1b4f90b72b9d4829591a
Mervin Fansler <mmfansler@gmail.com>
2020-10-10 01:56:46 -0400

    clean tar

--- examples/neuron_1k_v3_fastq/download.sh
+++ examples/neuron_1k_v3_fastq/download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env #!/usr/bin/env bash
 
 wget -O tmp.tar https://cf.10xgenomics.com/samples/cell-exp/3.0.0/neuron_1k_v3/neuron_1k_v3_fastqs.tar \
-  && tar -xvzf tmp.tar \
+  && tar -xf tmp.tar \
   && mv neuron_1k_v3_fastqs fastq \
   && rm tmp.tar
