fdd3256a787a9d720c44c10d636bcc6fb975791e
Mervin Fansler <mmfansler@gmail.com>
2020-10-12 04:23:13 -0400

    correct whitelist; fixes #11

--- extdata/bxs/download_10X_whitelists.sh
+++ extdata/bxs/download_10X_whitelists.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # 10xv1
-wget https://github.com/10XGenomics/supernova/raw/1.2.0/tenkit/lib/python/tenkit/barcodes/737K-april-2014_rc.txt
+wget https://github.com/10XGenomics/cellranger/raw/3.0.2/lib/python/cellranger/barcodes/737K-april-2014_rc.txt
 
 # 10xv2
-wget https://github.com/10XGenomics/supernova/raw/1.2.0/tenkit/lib/python/tenkit/barcodes/737K-august-2016.txt
+wget https://github.com/10XGenomics/cellranger/raw/3.0.2/lib/python/cellranger/barcodes/737K-august-2016.txt
 
 # 10xv3
-wget https://github.com/10XGenomics/supernova/raw/1.2.0/tenkit/lib/python/tenkit/barcodes/4M-with-alts-february-2016.txt
+wget https://github.com/10XGenomics/cellranger/raw/3.0.2/lib/python/cellranger/barcodes/3M-february-2018.txt.gz \
+    && gzip -d 3M-february-2018.txt.gz
+
