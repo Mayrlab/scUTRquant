65c8db38d35a8772c34482976599529c3217b730
Mervin Fansler <mmfansler@gmail.com>
2021-03-12 00:13:19 -0500

    updated example

--- examples/neuron_1k_v2_bam/download.sh
+++ examples/neuron_1k_v2_bam/download.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-wget -O tmp.tar https://cf.10xgenomics.com/samples/cell-exp/3.0.0/neuron_1k_v2/neuron_1k_v2_fastqs.tar \
-  && tar -xf tmp.tar \
-  && mv neuron_1k_v2_fastqs fastq \
-  && rm tmp.tar
+wget -O neuron_1k_v2.bam https://cf.10xgenomics.com/samples/cell-exp/3.0.0/neuron_1k_v2/neuron_1k_v2_possorted_genome_bam.bam
 
 # Download analysis data and make annots.csv
 wget -O tmp.tar.gz https://cf.10xgenomics.com/samples/cell-exp/3.0.0/neuron_1k_v2/neuron_1k_v2_analysis.tar.gz \
@@ -11,5 +8,5 @@ wget -O tmp.tar.gz https://cf.10xgenomics.com/samples/cell-exp/3.0.0/neuron_1k_v
     && rm tmp.tar.gz
 
 join -t',' analysis/clustering/graphclust/clusters.csv analysis/tsne/2_components/projection.csv | \
-    sed -E 's/([ACGT]{16})-1/neuron_1k_v2_fastq_\1/' | \
+    sed -E 's/([ACGT]{16})-1/neuron_1k_v2_bam_\1/' | \
     sed '1 s/^.*$/cell_id,cluster,tsne_1,tsne_2/' > annots.csv
