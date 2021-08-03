41c6658ab28fcc0b86b0669802a28cb4180cf480
Mervin Fansler <mmfansler@gmail.com>
2021-08-03 17:59:25 -0400

    closes #17

--- examples/heart_1k_v2_fastq/download.sh
+++ examples/heart_1k_v2_fastq/download.sh
@@ -4,3 +4,12 @@ wget -O tmp.tar https://cf.10xgenomics.com/samples/cell-exp/3.0.0/heart_1k_v2/he
   && tar -xf tmp.tar \
   && mv heart_1k_v2_fastqs fastq \
   && rm tmp.tar
+
+## Download analysis data and make annots.csv
+wget -O tmp.tar.gz https://cf.10xgenomics.com/samples/cell-exp/3.0.0/heart_1k_v2/heart_1k_v2_analysis.tar.gz \
+    && tar -xvzf tmp.tar.gz \
+    && rm tmp.tar.gz
+
+join -t',' analysis/clustering/graphclust/clusters.csv analysis/tsne/2_components/projection.csv | \
+    sed -E 's/([ACGT]{16})-1/heart_1k_v2_fastq_\1/' | \
+    sed '1 s/^.*$/cell_id,cluster,tsne_1,tsne_2/' > annots.csv
