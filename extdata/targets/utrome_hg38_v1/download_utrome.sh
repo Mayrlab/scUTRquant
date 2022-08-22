2c2e58f7f7376410295033b3b3956940da582f9b
Mervin Fansler <mmfansler@gmail.com>
2022-08-22 03:11:41 -0400

    basic annotations

--- extdata/targets/utrome_hg38_v1/download_utrome.sh
+++ extdata/targets/utrome_hg38_v1/download_utrome.sh
@@ -7,7 +7,7 @@ wget -O tmp.tar.gz "https://figshare.com/ndownloader/files/35874803?private_link
 
 # hg38 Annotations
 ## txs
-#wget -O utrome_txs_annotation.Rds
+wget -O utrome_txs_annotation.Rds "https://figshare.com/ndownloader/files/36741828?private_link=9f97fd9772574bee6a61"
 
 ## genes
-#wget -O utrome_genes_annotation.Rds
+wget -O utrome_genes_annotation.Rds "https://figshare.com/ndownloader/files/36741825?private_link=9f97fd9772574bee6a61"
