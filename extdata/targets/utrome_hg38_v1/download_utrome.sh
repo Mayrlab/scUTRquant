a7279bb28d87f317d15717e0a1520e0b9aaeabd8
Mervin M Fansler <mmfansler@gmail.com>
2026-01-25 22:49:47 +0100

    fix: switch from Figshare urls to GitHub
    fixes #100

--- extdata/targets/utrome_hg38_v1/download_utrome.sh
+++ extdata/targets/utrome_hg38_v1/download_utrome.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # hg38 UTRome 2022.06
-wget -O tmp.tar.gz "https://figshare.com/ndownloader/files/35874803?private_link=9f97fd9772574bee6a61" \
+wget -O tmp.tar.gz "https://github.com/Mayrlab/hcl-utrome/releases/download/v1.0.0/utrome.e30.t5.gc39.pas3.f0.9999.w500.tar.gz" \
     && tar -xvzf tmp.tar.gz \
     && rm tmp.tar.gz
 
 # hg38 Annotations
 ## txs
-wget -O utrome_hg38_v1_tx_annots.2024.05.13.csv.gz "https://figshare.com/ndownloader/files/46407826?private_link=9f97fd9772574bee6a61"
-wget -O utrome_hg38_v1_tx_annots.2024.05.13.Rds "https://figshare.com/ndownloader/files/46407829?private_link=9f97fd9772574bee6a61"
+wget -O utrome_hg38_v1_tx_annots.2024.05.13.csv.gz "https://github.com/Mayrlab/atlas-hs/releases/download/v0.1.0/utrome_hg38_v1_tx_annots.2024.05.13.csv.gz"
+wget -O utrome_hg38_v1_tx_annots.2024.05.13.Rds "https://github.com/Mayrlab/atlas-hs/releases/download/v0.1.0/utrome_hg38_v1_tx_annots.2024.05.13.Rds"
 
 ## genes
-wget -O utrome_hg38_v1_gene_annots.2024.05.13.csv.gz "https://figshare.com/ndownloader/files/46407820?private_link=9f97fd9772574bee6a61"
-wget -O utrome_hg38_v1_gene_annots.2024.05.13.Rds "https://figshare.com/ndownloader/files/46407823?private_link=9f97fd9772574bee6a61"
+wget -O utrome_hg38_v1_gene_annots.2024.05.13.csv.gz "https://github.com/Mayrlab/atlas-hs/releases/download/v0.1.0/utrome_hg38_v1_gene_annots.2024.05.13.csv.gz"
+wget -O utrome_hg38_v1_gene_annots.2024.05.13.Rds "https://github.com/Mayrlab/atlas-hs/releases/download/v0.1.0/utrome_hg38_v1_gene_annots.2024.05.13.Rds"
--- extdata/targets/utrome_mm10_v2/download_utrome.sh
+++ extdata/targets/utrome_mm10_v2/download_utrome.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # mm10 UTRome 2022.08
-wget -O tmp.tar.gz "https://figshare.com/ndownloader/files/36677958?private_link=78bbfa3c5e58a8cc5275" \
+wget -O tmp.tar.gz "https://github.com/Mayrlab/mca-utrome/releases/download/v2.0.1/utrome.e30.t5.gc25.pas3.f0.9999.w500.tar.gz" \
     && tar -xvzf tmp.tar.gz \
     && rm tmp.tar.gz
 
 # mm10 Annotations
 ## txs
-wget -O utrome_mm10_v2_tx_annots.2024.05.13.csv.gz "https://figshare.com/ndownloader/files/46407730?private_link=78bbfa3c5e58a8cc5275"
-wget -O utrome_mm10_v2_tx_annots.2024.05.13.Rds "https://figshare.com/ndownloader/files/46407733?private_link=78bbfa3c5e58a8cc5275"
+wget -O utrome_mm10_v2_tx_annots.2024.05.13.csv.gz "https://github.com/Mayrlab/atlas-mm/releases/download/v0.2.0/utrome_mm10_v2_tx_annots.2024.05.13.csv.gz"
+wget -O utrome_mm10_v2_tx_annots.2024.05.13.Rds "https://github.com/Mayrlab/atlas-mm/releases/download/v0.2.0/utrome_mm10_v2_tx_annots.2024.05.13.Rds"
 
 ## genes
-wget -O utrome_mm10_v2_gene_annots.2024.05.13.csv.gz "https://figshare.com/ndownloader/files/46407724?private_link=78bbfa3c5e58a8cc5275"
-wget -O utrome_mm10_v2_gene_annots.2024.05.13.Rds "https://figshare.com/ndownloader/files/46407727?private_link=78bbfa3c5e58a8cc5275"
+wget -O utrome_mm10_v2_gene_annots.2024.05.13.csv.gz "https://github.com/Mayrlab/atlas-mm/releases/download/v0.2.0/utrome_mm10_v2_gene_annots.2024.05.13.csv.gz"
+wget -O utrome_mm10_v2_gene_annots.2024.05.13.Rds "https://github.com/Mayrlab/atlas-mm/releases/download/v0.2.0/utrome_mm10_v2_gene_annots.2024.05.13.Rds"
